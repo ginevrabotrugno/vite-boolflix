74b09e0aca0a8813a6c1e1f28d176991e1abeb37
ginevrabotrugno <ginevrabotrugno.gb@gmail.com>
2024-07-11 11:49:54 +0200

    build: set vite

---
+++ src/store.js
@@ -0,0 +1,5 @@
+import { reactive } from 'vue'
+
+export const store = reactive ({
+    apiURL: 'https://api.themoviedb.org/3/search/movie?api_key=6d80c7fc6fb3c8cde8ea957eacd0ae7c&language=it-IT&query=',
+});
