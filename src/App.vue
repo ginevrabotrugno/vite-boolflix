2e3cf7c9b5e2dec5bf144049ae447aa32e540932
ginevrabotrugno <ginevrabotrugno.gb@gmail.com>
2024-07-11 12:40:39 +0200

    feat: print basic values of found movies

--- src/App.vue
+++ src/App.vue
@@ -25,7 +25,7 @@ export default {
       axios.get(endpoint)
       .then(res => {
         console.log(res.data.results);
-        store.results = res.data.results;
+        store.movieList = res.data.results;
         store.searchMovie = '';
       })
       .catch(err => {
@@ -44,9 +44,16 @@ export default {
 
   <div class="searchbar">
     <form action="#">
-      <input type="text" v-model="store.searchMovie">
+      <input type="text" v-model="store.searchMovie" @key.enter="search()">
       <button @click.prevent="search()">Search</button>
     </form>
+
+    <ul v-for="(movie, i) in store.movieList" :key="i">
+      <li>{{ movie.title }}</li>
+      <li>{{ movie.original_title }}</li>
+      <li> {{ movie.original_language }}</li>
+      <li> {{ movie.vote_average }} </li>
+    </ul>
   </div>
   
 </template>
--- src/store.js
+++ src/store.js
@@ -3,6 +3,6 @@ import { reactive } from 'vue'
 export const store = reactive ({
     apiURL: 'https://api.themoviedb.org/3/search/movie?api_key=6d80c7fc6fb3c8cde8ea957eacd0ae7c&language=it-IT&query=',
     searchMovie: "",
-    results: [],
+    movieList: [],
 
 });
