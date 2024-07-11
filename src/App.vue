f49c3533b400aa2bba117f0ddfd9510ca4593a43
ginevrabotrugno <ginevrabotrugno.gb@gmail.com>
2024-07-11 17:40:22 +0200

    feat: create AppContent component

--- src/App.vue
+++ src/App.vue
@@ -10,12 +10,14 @@ import "/node_modules/flag-icons/css/flag-icons.min.css";
 
 // import components
 import AppHeader from './components/AppHeader.vue';
+import AppContent from './components/AppContent.vue';
 
 
 export default {
   name: "App",
   components: {
-    AppHeader
+    AppHeader,
+    AppContent,
   },
   data() {
     return {
@@ -52,37 +54,6 @@ export default {
       })
 
     },
-    getFlagClass(language) {
-      // Mappa delle lingue alle classi delle bandiere
-      const languageMap = {
-        en: 'us',   // Inglese
-        it: 'it',   // Italiano
-        fr: 'fr',   // Francese
-        es: 'es',   // Spagnolo
-        de: 'de',   // Tedesco
-        ru: 'ru',   // Russo
-        ja: 'jp',   // Giapponese
-        ko: 'kr',   // Coreano
-        zh: 'cn',   // Cinese
-        pt: 'pt',   // Portoghese
-        ar: 'sa',   // Arabo
-        hi: 'in',   // Hindi
-        nl: 'nl',   // Olandese
-        sv: 'se',   // Svedese
-        da: 'dk',   // Danese
-        fi: 'fi',   // Finlandese
-        no: 'no',   // Norvegese
-        pl: 'pl',   // Polacco
-        tr: 'tr',   // Turco
-        el: 'gr',   // Greco
-        he: 'il',   // Ebraico
-        th: 'th',   // Thailandese
-        vi: 'vn',   // Vietnamita
-        id: 'id',   // Indonesiano
-        ms: 'my',   // Malese
-      };
-      return languageMap[language] || 'un'; // 'un' per mostrare un'icona di fallback
-    }
   },
   created() {
     this.searchTitle();
@@ -96,37 +67,7 @@ export default {
 
   <main>
 
-    <div>
-      <h2>MOVIES</h2>
-      <ul v-for="(movie, i) in store.movieList" :key="i">
-        <li class="thumb">
-          <img :src="`https://image.tmdb.org/t/p/w342${movie.poster_path}`" :alt="movie.title">
-        </li>
-        <li>{{ movie.title }}</li>
-        <li>{{ movie.original_title }}</li>
-        <li>
-          <span :class="`fi fi-${getFlagClass(movie.original_language)}`"></span>
-        </li>
-        <li> {{ movie.vote_average }} </li>
-      </ul>
-
-    </div>
-
-    <div>
-      <h2>TV SERIES</h2>
-      <ul v-for="(tvSeries, i) in store.tvSeriesList" :key="i">
-        <li class="thumb">
-          <img :src="`https://image.tmdb.org/t/p/w342${tvSeries.poster_path}`" :alt="tvSeries.name">
-        </li>
-        <li>{{ tvSeries.name }}</li>
-        <li>{{ tvSeries.original_name }}</li>
-        <li>
-          <span :class="`fi fi-${getFlagClass(tvSeries.original_language)}`"></span>
-        </li>
-        <li> {{ tvSeries.vote_average }} </li>
-      </ul>
-
-    </div>
+    <AppContent/>
 
   </main>
 
---
+++ src/components/AppContent.vue
@@ -0,0 +1,99 @@
+<script>
+// import dello store
+import {store} from '../store';
+
+export default {
+    name: 'Appcontent',
+    data() {
+        return {
+            store
+        }
+    },
+    methods: {
+        getFlagClass(language) {
+        // Mappa delle lingue alle classi delle bandiere
+        const languageMap = {
+            en: 'us',   // Inglese
+            it: 'it',   // Italiano
+            fr: 'fr',   // Francese
+            es: 'es',   // Spagnolo
+            de: 'de',   // Tedesco
+            ru: 'ru',   // Russo
+            ja: 'jp',   // Giapponese
+            ko: 'kr',   // Coreano
+            zh: 'cn',   // Cinese
+            pt: 'pt',   // Portoghese
+            ar: 'sa',   // Arabo
+            hi: 'in',   // Hindi
+            nl: 'nl',   // Olandese
+            sv: 'se',   // Svedese
+            da: 'dk',   // Danese
+            fi: 'fi',   // Finlandese
+            no: 'no',   // Norvegese
+            pl: 'pl',   // Polacco
+            tr: 'tr',   // Turco
+            el: 'gr',   // Greco
+            he: 'il',   // Ebraico
+            th: 'th',   // Thailandese
+            vi: 'vn',   // Vietnamita
+            id: 'id',   // Indonesiano
+            ms: 'my',   // Malese
+        };
+        return languageMap[language] || 'un'; // 'un' per mostrare un'icona di fallback
+        }
+
+    }
+}
+</script>
+
+<template>
+    <section>
+           
+        <h2>MOVIES</h2>
+
+        <div class="thumb" v-for="(movie, i) in store.movieList" :key="i">
+
+            <div>
+                <img :src="`https://image.tmdb.org/t/p/w342${movie.poster_path}`" :alt="movie.title">
+            </div>
+
+            <ul class="info">
+                <li><strong>Titolo: </strong>{{ movie.title }}</li>
+                <li><strong>Titolo Originale: </strong>{{ movie.original_title }}</li>
+                <li>
+                    <span :class="`fi fi-${getFlagClass(movie.original_language)}`"></span>
+                </li>
+                <li><strong>Voto: </strong> {{ movie.vote_average }} </li>
+            </ul>
+        </div>
+
+    </section>
+
+    <section>
+
+        <h2>TV SERIES</h2>
+
+        <div class="thumb" v-for="(tvSeries, i) in store.tvSeriesList" :key="i">
+
+            <img :src="`https://image.tmdb.org/t/p/w342${tvSeries.poster_path}`" :alt="tvSeries.name">
+
+            <ul class="info">
+                <li><strong>Titolo:</strong>{{ tvSeries.name }}</li>
+                <li><strong>Titolo Originale: </strong>{{ tvSeries.original_name }}</li>
+                <li>
+                    <span :class="`fi fi-${getFlagClass(tvSeries.original_language)}`"></span>
+                </li>
+                <li><strong>Voto: </strong> {{ tvSeries.vote_average }} </li>
+            </ul>
+
+        </div>
+
+    </section>
+
+</template>
+
+<style scoped lang="scss">
+@use '../style/partials/variables' as *;
+
+
+</style>
